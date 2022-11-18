75d9f91193cb5e62e9b30dd7814b62cea7a0f51f
Manuela Garcia <mgh9517@gmail.com>
2022-11-18 08:48:27 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import * as Realm from 'realm-web'
+import { useEffect, useState } from 'react'
+import _ from 'lodash'
 
-function App() {
+import './App.css'
+
+const app = new Realm.App({ id: process.env.REACT_APP_MONGO_APP_ID })
+
+function App () {
+  const [user, setUser] = useState()
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    const login = async () => {
+      // Authenticate anonymously
+      const user = await app.logIn(Realm.Credentials.anonymous())
+      setUser(user)
+
+      // Connect to the database
+      const mongodb = app.currentUser.mongoClient()
+      const collection = mongodb.db(process.env.REACT_APP_MONGO_DB).collection(process.env.REACT_APP_MONGO_COLLECTION)
+
+      // Everytime a change happens in the stream, add it to the list of events
+      for await (const change of collection.watch()) {
+        if (change.operationType === 'update') {
+          setEvents(prevData => {
+          // Make sure we don't add the same event twice
+            if (prevData.length === 0 || (_.find(prevData, change) === undefined)) {
+              return [...prevData, change]
+            }
+            return prevData
+          })
+        }
+      }
+    }
+    login()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {!!user &&
+        <div className='App-header'>
+          <h1>Connected as user ${user.id}</h1>
+          <div>
+            <p>Latest events:</p>
+            <table>
+              <thead>
+                <tr>
+                  <td>Operation</td>
+                  <td>Document Key</td>
+                  <td>Full Document</td>
+                </tr>
+              </thead>
+              <tbody>
+                {events.map((e, i) => (
+                  <tr key={i}>
+                    <td>{e.operationType}</td>
+                    <td>{e.documentKey._id.toString()}</td>
+                    <td>{JSON.stringify(e.fullDocument)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
